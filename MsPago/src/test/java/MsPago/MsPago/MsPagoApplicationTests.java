4daa4390fc5bb8b44a285c15527dad0fe0e286d7
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2024-12-09 22:45:00 -0500

    microservicio pago

---
+++ MsPago/src/test/java/MsPago/MsPago/MsPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package MsPago.MsPago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
