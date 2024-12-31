88482107693b51ad9dabbf5dca0e28ef94a889ee
jairowong <jairozz200@gmail.com>
2024-12-31 09:50:49 -0500

    asd
    asd

---
+++ MsGestionMatricula/ControllerMatricula.java
@@ -0,0 +1,13 @@
+package MsMatricula.MsGestionMatricula.controller;
+
+import org.springframework.stereotype.Controller;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Controller
+@Entity
+@Table
+public class ControllerMatricula {
+
+}
